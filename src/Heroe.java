9d4b5742917fda8fbe23af4d8a85307d06670d84
erick <erickchuqui883@gmail.com>
2025-05-01 14:55:22 -0500

    git init git add .

--- src/Heroe.java
+++ src/Heroe.java
@@ -6,7 +6,7 @@ public class Heroe {
 
     public Heroe() {
         this.nombre = "Spider-Man";
-        this.raza = "Meta-Humano";
+        this.raza = "Humano";
         this.nivelPoder = 80;
         this.habilidad = "Agilidad";
     }
